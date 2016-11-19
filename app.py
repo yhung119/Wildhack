a03ce88dbcf9993ddb3dfb70a6a88797a93f5237
yhung119 <yhung119@illinois.edu>
2016-11-19 08:52:19 -0600

    connect to html

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 
 #first comment
-@app.route('/user')
+@app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run()
