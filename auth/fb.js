9b91987a916206fb63eaa816e6de7acecb5ef4cb
yhung119 <yhung119@illinois.edu>
2016-11-20 03:15:51 -0600

    change url for heroku

--- auth/fb.js
+++ auth/fb.js
@@ -1,5 +1,6 @@
 module.exports = {
 	'appID' : '616139055240150',
 	'appSecret' : 'edff0c7b74e64d5a33140e380806d8e1',
-	'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
+	'callbackUrl' : 'https://shrouded-castle-28111.herokuapp.com/facebook/callback'
 }
+//'http://localhost:3000/login/facebook/callback'
